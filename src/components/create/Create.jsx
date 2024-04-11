f4620954604df29ce3de4d3527d9984a0ccdfc49
Andres Flores <andresflo883@gmail.com>
2024-04-11 17:54:18 -0400

    added success and error dialogs to Create

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,38 +1,69 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import axios from 'axios';
 import '../../styles/create.scss';
+import { Toast } from 'primereact/toast';
 
 const Create = () => {
   const [name, setName] = useState('');
+  const toast = useRef(null);
+
+  const success = () => {
+    toast.current.show({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Record created successfully',
+      life: 3000
+    });
+  };
+
+  const showErrorMessage = () => {
+    toast.current.show({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Error creating record',
+      life: 3000
+    });
+  };
 
   const createNewPerson = () => {
-    if (!name) return
+    if (!name) return;
 
-    axios.post('https://test.epdet.org/api/applicant', {
-      name: name,
-      date: new Date(),
-      check: false
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
+    axios
+      .post('https://test.epdet.org/api/applicant', {
+        name: name,
+        date: new Date(),
+        check: false
+      })
+      .then(function (response) {
+        success();
+        console.log(response);
+        setName('')
+      })
+      .catch(function (error) {
+        showErrorMessage();
+        console.log(error);
+        setName('')
+
+      });
+  };
 
   return (
-    <div className="create-wrapper">
-      <div>Name</div>
+    <>
+      <Toast ref={toast} />
 
-      <InputText
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Button onClick={createNewPerson} label="Create" severity="success"/>
-    </div>
+      <div className="create-wrapper">
+        <div>Name</div>
+
+        <InputText
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button onClick={createNewPerson} label="Create" severity="success" />
+      </div>
+    </>
   );
 };
 
