462c10ecc65f08b12098c65dd35e617692aadf78
Andres Flores <andresflo883@gmail.com>
2024-04-12 14:30:38 -0400

    added single person delete and search function

--- src/components/people/All.jsx
+++ src/components/people/All.jsx
@@ -3,6 +3,7 @@ import { Checkbox } from 'primereact/checkbox';
 import axios from 'axios';
 import '../../styles/all.scss';
 import { Route, Link } from 'react-router-dom';
+import { InputText } from 'primereact/inputtext';
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -10,6 +11,8 @@ function sleep(ms) {
 
 const All = () => {
   const [people, setPeople] = useState();
+  const [searchValue, setSearchValue] = useState('');
+  const [regex, setRegex] = useState(/(.*?)/)
 
   useEffect(() => {
     async function onMount() {
@@ -21,32 +24,55 @@ const All = () => {
     onMount();
   }, []);
 
+  useEffect(() => {
+    if (!searchValue) return;
+
+    // console.log(searchValue);
+    setRegex(new RegExp(searchValue))
+  }, [searchValue]);
+
   return (
     <div className="all-people-wrapper">
       {people ? (
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Date</th>
-              <th>Check</th>
-            </tr>
+        <div className="table-wrapper">
+          <div className="search-wrapper">
+            <div>Search</div>
+            <InputText
+              className="w-full"
+              // required
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+          </div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Check</th>
+              </tr>
 
-            {people.map((person, i) => {
-              return (
-                <tr className="person-row" key={i}>
-                  <td>
-                    <Link to={`/people?id=${person._id}`}>{person.name}</Link>
-                  </td>
-                  <td>{person.date}</td>
-                  <td>
-                    <Checkbox checked={person.check}></Checkbox>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              {people.map((person, i) => {
+                if (person.name.match(searchValue)) {
+                  return (
+                    <tr className="person-row" key={i}>
+                      <td>
+                        <Link to={`/people?id=${person._id}`}>{person.name}</Link>
+                      </td>
+                      <td>{person.date}</td>
+                      <td>
+                        <Checkbox checked={person.check}></Checkbox>
+                      </td>
+                    </tr>
+                  );
+                } else {
+                  return null
+                }
+                
+              })}
+            </tbody>
+          </table>
+        </div>
       ) : (
         <p className="loading-text">Loading...</p>
       )}
--- src/components/people/Person.jsx
+++ src/components/people/Person.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams,useNavigate } from 'react-router-dom';
 import '../../styles/person.scss';
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
+import { Toast } from 'primereact/toast';
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -13,6 +14,8 @@ const Person = () => {
   const [person, setPerson] = useState();
   const [searchParams, setSearchParams] = useSearchParams();
   const id = searchParams.get('id');
+  const toast = useRef(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!id) return;
@@ -28,37 +31,82 @@ const Person = () => {
     onMount();
   }, [id]);
 
+  const success = () => {
+    toast.current.show({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Record deleted successfully',
+      life: 3000
+    });
+  };
+
+  const showErrorMessage = () => {
+    toast.current.show({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Error deleting record',
+      life: 3000
+    });
+  };
+
+  const deletePerson = () => {
+    if (!id) return;
+
+    axios
+      .delete(`https://test.epdet.org/api/applicant/?id=${id}`)
+      .then(function (response) {
+        // success();
+        // console.log(response);
+        // setId('')
+        // Navigate('')
+        navigate("/");
+      })
+      .catch(function (error) {
+        showErrorMessage();
+        // console.log(error);
+        // setId('')
+      });
+  };
+
   return (
-    <div className="person-wrapper">
-      {person ? (
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Date</th>
-              <th>Check</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-            <tr>
-              <td>{person.name}</td>
-              <td>{person.date}</td>
-              <td>
-                <Checkbox checked={person.check}></Checkbox>
-              </td>
-              <td>
-                <Button severity="primary" disabled>Edit</Button>
-              </td>
-              <td>
-                <Button severity="danger" disabled>Delete</Button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      ) : (
-        <p className="loading-text">Loading...</p>
-      )}
-    </div>
+    <>
+      <Toast ref={toast} />
+
+      <div className="person-wrapper">
+        {person ? (
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Check</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+              <tr>
+                <td>{person.name}</td>
+                <td>{person.date}</td>
+                <td>
+                  <Checkbox checked={person.check}></Checkbox>
+                </td>
+                <td>
+                  <Button severity="primary" disabled>
+                    Edit
+                  </Button>
+                </td>
+                <td>
+                  <Button onClick={deletePerson} severity="danger">
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <p className="loading-text">Loading...</p>
+        )}
+      </div>
+    </>
   );
 };
 
