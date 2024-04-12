da49560e75043ee44eda6518db917b70d228a981
Andres Flores <andresflo883@gmail.com>
2024-04-11 20:33:30 -0400

    added loading screen text

--- src/components/people/All.jsx
+++ src/components/people/All.jsx
@@ -4,40 +4,52 @@ import axios from 'axios';
 import '../../styles/all.scss';
 import { Route, Link } from 'react-router-dom';
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const All = () => {
-  const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState();
 
   useEffect(() => {
-    axios.get('https://test.epdet.org/api/applicant').then((response) => {
-      setPeople(response.data);
-    });
+    async function onMount() {
+      await sleep(1000);
+      axios.get('https://test.epdet.org/api/applicant').then((response) => {
+        setPeople(response.data);
+      });
+    }
+    onMount();
   }, []);
 
   return (
     <div className="all-people-wrapper">
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Check</th>
-          </tr>
+      {people ? (
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Check</th>
+            </tr>
 
-          {people.map((person, i) => {
-            return (
-              <tr className="person-row" key={i}>
-                <td>
-                  <Link to={`/people?id=${person._id}`}>{person.name}</Link>
-                </td>
-                <td>{person.date}</td>
-                <td>
-                  <Checkbox checked={person.check}></Checkbox>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            {people.map((person, i) => {
+              return (
+                <tr className="person-row" key={i}>
+                  <td>
+                    <Link to={`/people?id=${person._id}`}>{person.name}</Link>
+                  </td>
+                  <td>{person.date}</td>
+                  <td>
+                    <Checkbox checked={person.check}></Checkbox>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <p className="loading-text">Loading...</p>
+      )}
     </div>
   );
 };
--- src/components/people/Person.jsx
+++ src/components/people/Person.jsx
@@ -5,47 +5,59 @@ import '../../styles/person.scss';
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const Person = () => {
-  const [person, setPerson] = useState({});
-  let [searchParams, setSearchParams] = useSearchParams();
+  const [person, setPerson] = useState();
+  const [searchParams, setSearchParams] = useSearchParams();
   const id = searchParams.get('id');
 
   useEffect(() => {
     if (!id) return;
 
-    axios
-      .get(`https://test.epdet.org/api/applicant/?id=${id}`)
-      .then((response) => {
-        setPerson(response.data);
-      });
+    async function onMount() {
+      await sleep(1000);
+      axios
+        .get(`https://test.epdet.org/api/applicant/?id=${id}`)
+        .then((response) => {
+          setPerson(response.data);
+        });
+    }
+    onMount();
   }, [id]);
 
   return (
     <div className="person-wrapper">
-      <table>
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Check</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-          <tr>
-            <td>{person.name}</td>
-            <td>{person.date}</td>
-            <td>
-              <Checkbox checked={person.check}></Checkbox>
-            </td>
-            <td>
-              <Button severity="primary">Edit</Button>
-            </td>
-            <td>
-              <Button severity="danger">Delete</Button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {person ? (
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Check</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+            <tr>
+              <td>{person.name}</td>
+              <td>{person.date}</td>
+              <td>
+                <Checkbox checked={person.check}></Checkbox>
+              </td>
+              <td>
+                <Button severity="primary">Edit</Button>
+              </td>
+              <td>
+                <Button severity="danger">Delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <p className="loading-text">Loading...</p>
+      )}
     </div>
   );
 };
